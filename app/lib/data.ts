58c210065611a1279681e0b7fa57380466281ff5
vvnab <vvnab@mail.ru>
2024-03-28 12:25:08 +0300

    ащкь аштшыр

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,13 +18,9 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
-
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -45,7 +41,8 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
